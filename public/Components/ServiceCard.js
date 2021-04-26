67a9959b31040568b25f0b3961b7bce2aab30279
JoaquinManchenoP <manchenojj@g.cofc.edu>
2021-04-26 09:47:56 -0400

    services section

--- public/Components/ServiceCard.js
+++ public/Components/ServiceCard.js
@@ -1,9 +1,11 @@
 export default function ServiceCard({ serviceName, serviceDescription }) {
   return (
-    <div className="text-lg w-48 h-full bg-red-400 space-y-5 flex flex-col items-center justify-center">
-      <div className="cardContent w-full h-xs bg-purple-400 space-y-3 flex flex-col items-center ">
-        <div className="cardtitle text-xl">{serviceName}</div>
-        <div className="serviceDescription text-lg">{serviceDescription}</div>
+    <div className="text-lg w-48 h-full bg-gray-300 space-y-5 flex flex-col items-center justify-center rounded-xl ">
+      <div className="cardContent w-full h-xxs  space-y-5 flex flex-col items-center   ">
+        <div className="cardtitle text-xl ">{serviceName}</div>
+        <div className="serviceDescription text-md font-light">
+          {serviceDescription}
+        </div>
       </div>
     </div>
   );
--- public/Components/Services.js
+++ public/Components/Services.js
@@ -1,26 +1,52 @@
 import ServiceCard from "./ServiceCard";
+import { GoTools } from "react-icons/go";
+import { AiOutlineFieldTime } from "react-icons/ai";
+import { TiLocation } from "react-icons/ti";
 
 export default function Services() {
   return (
-    <div className="servicesSection h-xl w-screen flex items-center justify-center">
-      <div className="servocesSectionContent h-lg w-3/5 bg-purple-400 flex flex-col items-center justify-center space-y-5">
-        <div className="title text-2xl ">
+    <div className="servicesSection h-full w-screen flex items-center justify-center mt-20">
+      <div className="servocesSectionContent h-lg  xs:w-full sm:w-full md:w-3/5 flex flex-col items-center justify-center space-y-5 ">
+        <div className="title text-2xl  ">
           <p>Nuestros Servicios</p>
         </div>
-        <div className="serviceDescription w-2/3">
+        <div className="serviceDescription w-2/3 ">
           <p className="text-md">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
             laudantium accusamus adipisci consectetur neque excepturi doloribus
             molestiae, natus maiores sed.
           </p>
         </div>
-        <div className="serviceIcons h-sm w-full bg-green-400 flex justify-center space-x-16">
-          <ServiceCard
-            serviceName="Reparaciones"
-            serviceDescription="The fix the arrgle todos los aparatod electronicos mi nepas"
-          />
-          <ServiceCard />
-          <ServiceCard />
+        <div className="serviceIcons h-sm w-full flex justify-center pt-6 ">
+          <div className="cards xs:space-x-0 sm:space-x-6 md:space-x-16 flex">
+            <div className="card1">
+              <div className="icon w-full flex items-center  justify-center text-blue-700">
+                <GoTools size={50} />
+              </div>
+              <ServiceCard
+                serviceName="Reparaciones"
+                serviceDescription="The fix the arrgle todos los aparatod electronicos mi nepas"
+              />
+            </div>
+            <div className="card2">
+              <div className="icon w-full flex items-center justify-center text-blue-700">
+                <AiOutlineFieldTime size={50} />
+              </div>
+              <ServiceCard
+                serviceName="Atencion 24/7"
+                serviceDescription="Atencion permanente, todos los dias a cualquier hora"
+              />
+            </div>
+            <div className="card3">
+              <div className="icon w-full flex items-center justify-center text-blue-700">
+                <TiLocation size={50} />
+              </div>
+              <ServiceCard
+                serviceName="A domicilio"
+                serviceDescription="Servicio a domicilio dentro y fuera de la ciudad"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
